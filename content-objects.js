2392887e9ca86ae6b194a732b3e13f75590b9804
Diego <diannewdev@gmail.com>
2024-05-28 19:06:42 -0400

    initial commit

---
+++ content-objects.js
@@ -0,0 +1,46 @@
+let apresentcao = {
+    texto: 'Bem-vindo(a)! Sou Ana Clara Oliveira, psicóloga especializada em terapia cognitivo-comportamental, com 10 anos de experiência ajudando indivíduos a superar desafios emocionais e a encontrar um caminho para uma vida mais equilibrada e satisfatória. Acredito no poder da empatia e da escuta ativa para criar um ambiente seguro e acolhedor onde você possa explorar suas emoções, entender seus comportamentos e desenvolver estratégias eficazes para lidar com as dificuldades. Meu compromisso é oferecer um suporte personalizado e baseado em evidências para promover o seu bem-estar mental e emocional. Vamos juntos nessa jornada de autodescoberta e crescimento!',
+    fotoDePerfil: 'images/fotoDePerfil.jpg',
+    fotoApresentacao1: 'images/fotoApresentacao1.jpg',
+    fotoApresentacao2: 'images/fotoApresentacao2.jpg',
+    fotoApresentacao3: 'images/fotoApresentacao3.jpg',
+    fotoApresentacao4: 'images/fotoApresentacao4.jpg',
+    h2Apresentcao1: 'CULTIVANDO NOVOS MOMENTOS, UMA SESSÃO POR VEZ',
+    h3Apresentcao1: 'CRIANÇAS',
+    h3Apresentcao2: 'JOVENS',
+    h3Apresentcao3: 'CASAIS',
+}
+
+//https://i.etsystatic.com/12498747/r/il/7cfb6c/3181179402/il_fullxfull.3181179402_buvw.jpg
+
+let servicos = {
+    h4: 'TERAPIA COGNITIVO-COMPORTAMENTAL',
+    h2: 'Desvendar O Passado, Compreender o Presente, Preservar o Futuro',
+    p: 'A Terapia Cognitivo-Comportamental (TCC) é uma abordagem terapêutica amplamente reconhecida por sua eficácia no tratamento de diversos problemas de saúde mental, incluindo depressão, ansiedade, transtornos alimentares e estresse pós-traumático. Um dos principais benefícios da TCC é seu foco em identificar e modificar padrões de pensamento negativos e comportamentos disfuncionais que contribuem para o sofrimento emocional. Ao trabalhar com um terapeuta, os indivíduos aprendem técnicas práticas para reestruturar pensamentos negativos, desenvolver habilidades de enfrentamento e melhorar a regulação emocional. A TCC é baseada em evidências científicas e oferece resultados duradouros, capacitando os pacientes a manejar seus sintomas de forma independente e a promover mudanças positivas em suas vidas. Além disso, a TCC é uma abordagem estruturada e orientada a objetivos, tornando-a uma opção de tratamento eficaz e eficiente para muitas pessoas.',
+    botao: 'AGENDAR UMA CONVERSA',
+    imagem: 'images/fotoServicos.jpg',
+}
+
+//https://res.cloudinary.com/dz209s6jk/image/upload/v1658144705/Challenges/fvv3coes3vm7ndnw6tml.jpg
+
+let reviews = {
+    h2: 'TESTEMUNHOS',
+    nome1: 'Jéssica Camargo',
+    cargo1: 'Professora',
+    estrelas1: 5,
+    text1: 'Ana Clara tem sido uma guia incrível na minha jornada de autodescoberta e cura. Desde o primeiro encontro, ela me fez sentir completamente à vontade e segura para compartilhar meus pensamentos e sentimentos. Sua abordagem empática e suas técnicas de terapia cognitivo-comportamental me ajudaram a identificar e mudar padrões de pensamento negativos que estavam me impedindo de viver plenamente. Hoje, me sinto mais confiante e resiliente, e devo muito do meu progresso à Ana Clara. Recomendo-a sem hesitação!',
+    nome2: 'Pedro Costa',
+    cargo2: 'Dentista',
+    estrelas2: 4,
+    text2: 'Sempre tive dificuldade em lidar com minha ansiedade, mas desde que comecei a me consultar com a Ana Clara, minha vida mudou para melhor. Ela é extremamente atenciosa e possui uma capacidade única de ouvir e entender os problemas. As sessões são sempre produtivas, e as ferramentas e técnicas que ela me ensinou foram fundamentais para melhorar meu bem-estar. Sinto-me mais preparado para enfrentar os desafios do dia a dia e sou muito grato por todo o apoio e orientação que recebi.',
+    nome3: 'Fernanda Medeiros',
+    cargo3: 'Dona de Casa',
+    estrelas3: 5,
+    text3: 'Ana Clara é uma profissional excepcional! Ela me ajudou a superar uma fase muito difícil da minha vida com sua abordagem compassiva e suas estratégias eficazes de TCC. O ambiente que ela cria durante as sessões é acolhedor e sem julgamentos, o que fez toda a diferença no meu tratamento. Suas orientações não só me ajudaram a lidar com meus problemas imediatos, mas também me equiparam com habilidades práticas para o futuro. Sem dúvida, ela foi uma peça-chave na minha recuperação e crescimento pessoal.',
+}
+
+//https://marketplace.canva.com/EAE4PN_YLiY/1/0/566w/canva-green-minimalist-property-client-testimony-flyer-tpGXk7rhLIo.jpg
+
+let contato = {
+    foto: 'images/fotoContato.jpg',
+}
---
+++ readme.md
@@ -0,0 +1,21 @@
+Objetivo: quero fazer uma landing page para um terapeuta.
+
+Problema: não sei o que fazer.
+
+1 - Estrutura:
+
+A - hero
+B - apresentacao
+C - tipo de terapia
+D - relatos
+E - footer
+
+2 - Estilo
+
+A - Sol
+B - Paleta de cores: amarelo
+C - Tema: atenção, empatia, esperança
+
+3 - Features
+
+A - 
